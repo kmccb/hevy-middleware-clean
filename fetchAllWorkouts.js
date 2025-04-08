7ea7a60edec7e63a8958cdc6967ec7ed6d2806d7
kmccb <tomscott2340@gmail.com>
2025-04-08 15:38:22 -0400

    Fix: load dotenv only in local/dev

--- fetchAllWorkouts.js
+++ fetchAllWorkouts.js
@@ -1,5 +1,8 @@
 // fetchAllWorkouts.js
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+  }
+  
 const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
