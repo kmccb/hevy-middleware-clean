d002e1c3c7fc22295a5265cc901a15591c644559
kmccb <tomscott2340@gmail.com>
2025-04-08 13:25:04 -0400

    .

--- index.js
+++ index.js
@@ -324,5 +324,5 @@ app.post("/autoplan", async (req, res) => {
 app.get("/", (req, res) => res.send("🏋️ CoachGPT Middleware is LIVE on port 10000"));
 
 app.listen(PORT, () => console.log("🏋️ CoachGPT Middleware is LIVE on port 10000"));
-console.log("🔑 HEVY_API_KEY =", process.env.HEVY_API_KEY);
+
 
