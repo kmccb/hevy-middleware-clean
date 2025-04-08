2e60cb6ca0fdba70aec9be99afabdbb3399a40b1
kmccb <tomscott2340@gmail.com>
2025-04-08 10:29:33 -0400

    Add exerciseService to fetch and cache Hevy templates

--- exerciseService.js
+++ exerciseService.js
@@ -1,1 +1,33 @@
-// TODO: implement or restore this file
+// exerciseService.js
+const axios = require("axios");
+const fs = require("fs");
+
+const HEVY_API_KEY = process.env.HEVY_API_KEY;
+const EXERCISE_CACHE_FILE = "exercise_templates.json";
+const HEVY_API_BASE = "https://api.hevyapp.com/v1";
+
+/**
+ * Fetches all exercise templates from Hevy and saves them locally for reuse.
+ */
+async function fetchAllExercises() {
+  const all = [];
+  let page = 1;
+  let done = false;
+
+  while (!done) {
+    const res = await axios.get(`${HEVY_API_BASE}/exercise_templates?page=${page}&pageSize=50`, {
+      headers: { "api-key": HEVY_API_KEY }
+    });
+
+    const { exercise_templates, page_count } = res.data;
+    all.push(...exercise_templates);
+
+    if (page >= page_count) done = true;
+    else page++;
+  }
+
+  fs.writeFileSync(EXERCISE_CACHE_FILE, JSON.stringify(all, null, 2));
+  return all;
+}
+
+module.exports = { fetchAllExercises };
